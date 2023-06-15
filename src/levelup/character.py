c67164ed5090c591b16d9a09a6c5885dd112959a
TenzinChonyi <tenzinchonyi@gmail.com>
2023-06-15 19:26:21 +0000

    Adding default case unit test for character.

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -2,4 +2,7 @@ class Character:
     name = ""
 
     def __init__(self, character_name):
-        self.name = character_name
+        if character_name:
+            self.name = character_name
+        else:
+            self.name = "Character"
--- tests/test_character.py
+++ tests/test_character.py
@@ -6,3 +6,15 @@ class TestCharacterInitWithName(TestCase):
         ARBITRARY_NAME = "MyName"
         testobj = Character(ARBITRARY_NAME)
         self.assertEqual(ARBITRARY_NAME, testobj.name)
+
+    def test_default_name_assignment(self):
+        testobj = Character("")
+        self.assertEqual("Character", testobj.name)
+
+
+#         variable = None
+
+# if variable is not None:
+#     print("Variable is not null")
+# else:
+#     print("Variable is null")
