d9c3c4d4b15e202d75e46aca30bc38294e9fa979
TenzinChonyi <tenzinchonyi@gmail.com>
2023-06-15 20:55:58 +0000

    adding getPosition test case

--- src/levelup/character.py
+++ src/levelup/character.py
@@ -1,8 +1,11 @@
 class Character:
     name = ""
-
+    DEFUALT_NAME = "Character"
     def __init__(self, character_name):
         if character_name:
             self.name = character_name
         else:
-            self.name = "Character"
+            self.name = self.DEFUALT_NAME
+
+    def getPosition():
+        return 1;
--- tests/test_character.py
+++ tests/test_character.py
@@ -1,5 +1,7 @@
 from unittest import TestCase
 from levelup.character import Character
+from levelup.position import Position
+
 
 class TestCharacterInitWithName(TestCase):
     def test_init(self):
@@ -10,3 +12,11 @@ class TestCharacterInitWithName(TestCase):
     def test_default_name_assignment(self):
         testobj = Character("")
         self.assertEqual("Character", testobj.name)
+
+    def test_getPosition_returns_a_value(self):
+        ARBITRARY_NAME = "MyName"
+        testobj = Character(ARBITRARY_NAME)
+        self.assertIsNotNone(testobj.getPosition)
+        
+
+        
