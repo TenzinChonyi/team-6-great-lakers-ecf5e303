6474a050c1083fd2457330cfa4217db140c7c34f
Ismail Giles <iagiles72@outlook.com>
2023-06-16 18:10:15 +0000

    adding test cases

--- src/levelup/gamemap.py
+++ src/levelup/gamemap.py
@@ -12,3 +12,20 @@ class GameMap:
         elif position.coordinate_Y > 9 or position.coordinate_Y < 0:
             return False
         return True
+
+    def calculate_position(self, current_position, direction):
+        new_position = Position(current_position.coordinate_X, current_position.coordinate_Y);
+        match (direction):
+            case direction.NORTH:
+                new_position.coordinate_Y += 1
+            case direction.SOUTH:
+                new_position.coordinate_Y -= 1            
+            case direction.EAST:
+                new_position.coordinate_X += 1            
+            case direction.WEST:
+                new_position.coordinate_X -= 1            
+
+        if self.validate_position(new_position):
+            return new_position
+
+        return current_position
--- tests/test_gamemap.py
+++ tests/test_gamemap.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from levelup.gamemap import GameMap
 from levelup.position import Position
+from levelup.controller import Direction
 
 class TestMapInitDetails(TestCase):
     def test_init_position(self):
@@ -21,3 +22,35 @@ class TestMapInitDetails(TestCase):
         map = GameMap()
         position = Position(2,9)
         self.assertTrue(map.validate_position(position))
+
+    def test_calculate_move_north(self):
+        map = GameMap()
+        current_position = Position(0,0)
+        direction = Direction.NORTH
+        calculated_position = map.calculate_position(current_position, direction)
+        self.assertEqual(calculated_position.coordinate_X, 0)
+        self.assertEqual(calculated_position.coordinate_Y, 1)
+
+    def test_calculate_move_south(self):
+        map = GameMap()
+        current_position = Position(0,1)
+        direction = Direction.SOUTH
+        calculated_position = map.calculate_position(current_position, direction)
+        self.assertEqual(calculated_position.coordinate_X, 0)
+        self.assertEqual(calculated_position.coordinate_Y, 0)
+
+    def test_calculate_move_east(self):
+        map = GameMap()
+        current_position = Position(0,0)
+        direction = Direction.EAST
+        calculated_position = map.calculate_position(current_position, direction)
+        self.assertEqual(calculated_position.coordinate_X, 1)
+        self.assertEqual(calculated_position.coordinate_Y, 0)
+
+    def test_calculate_move_west(self):
+        map = GameMap()
+        current_position = Position(1,0)
+        direction = Direction.WEST
+        calculated_position = map.calculate_position(current_position, direction)
+        self.assertEqual(calculated_position.coordinate_X, 0)
+        self.assertEqual(calculated_position.coordinate_Y, 0)
